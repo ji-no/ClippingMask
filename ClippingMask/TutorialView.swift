9289a7c9bf8cb94702a2bff7636166ad7f4b69e3
ji-no <jino.jpn@gmail.com>
2022-06-06 00:26:44 +0900

    TutorialView作成

---
+++ ClippingMask/TutorialView.swift
@@ -0,0 +1,123 @@
+//
+//  TutorialView.swift
+//  ClippingMask
+//  
+//  Created by ji-no on R 4/06/05
+//  
+//
+
+import UIKit
+
+class TutorialView: UIView {
+
+    var headerHeight: CGFloat = 0
+    var masked: Bool = false
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setUp()
+    }
+
+    func start(to toRect: CGRect) {
+        startMaskAnimation(to: toRect)
+    }
+
+    func clear() {
+        clearMaskAnimation()
+    }
+
+}
+
+extension TutorialView {
+
+    private func setUp() {
+        backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        isUserInteractionEnabled = false
+        isHidden = true
+    }
+
+    private func startMaskAnimation(to toRect: CGRect) {
+        guard masked == false else { return }
+        masked = true
+
+        isHidden = false
+
+        let fromPath = fromRectPath()
+        let toPath = toRectPath(toRect)
+
+        let maskLayer = CAShapeLayer()
+        maskLayer.frame = bounds
+        maskLayer.fillColor = UIColor.black.cgColor
+        maskLayer.path = fromPath.cgPath
+        maskLayer.fillRule = CAShapeLayerFillRule.evenOdd
+        layer.mask = maskLayer
+
+        let anim = CABasicAnimation(keyPath: "path")
+        anim.fromValue = maskLayer.path
+        anim.toValue = toPath.cgPath
+        anim.duration = 0.2
+        anim.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+
+        maskLayer.add(anim, forKey: nil)
+
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        CATransaction.setCompletionBlock({
+            maskLayer.path = toPath.cgPath
+            maskLayer.fillRule = CAShapeLayerFillRule.evenOdd
+        })
+        CATransaction.commit()
+    }
+
+    private func clearMaskAnimation() {
+        guard masked == true else { return }
+        masked = false
+
+        let toPath = fromRectPath()
+
+        let maskLayer = layer.mask as! CAShapeLayer
+
+        let anim = CABasicAnimation(keyPath: "path")
+        anim.fromValue = maskLayer.path
+        anim.toValue = toPath.cgPath
+        anim.duration = 0.2
+        anim.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+
+        maskLayer.add(anim, forKey: nil)
+
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        CATransaction.setCompletionBlock({
+            maskLayer.path = toPath.cgPath
+            maskLayer.fillRule = CAShapeLayerFillRule.evenOdd
+        })
+        CATransaction.commit()
+    }
+
+    private func fromRectPath() -> UIBezierPath {
+        let outerRectanglePath = UIBezierPath(rect: layer.bounds)
+        let headerPath = UIBezierPath(rect: CGRect(x: 0, y: 0, width: bounds.width, height: headerHeight))
+
+        let fromRect = CGRect(x: 0, y: headerHeight, width: bounds.width, height: bounds.height - headerHeight)
+        let path = UIBezierPath(roundedRect: fromRect, cornerRadius: 1)
+        path.append(headerPath)
+        path.append(outerRectanglePath)
+        return path
+    }
+
+    private func toRectPath(_ toRect: CGRect) -> UIBezierPath {
+        let outerRectanglePath = UIBezierPath(rect: layer.bounds)
+        let headerPath = UIBezierPath(rect: CGRect(x: 0, y: 0, width: bounds.width, height: headerHeight))
+
+        let path = UIBezierPath(roundedRect: toRect, cornerRadius: toRect.height/2)
+        path.append(headerPath)
+        path.append(outerRectanglePath)
+        return path
+    }
+
+}
--- ClippingMask/ViewController.swift
+++ ClippingMask/ViewController.swift
@@ -12,11 +12,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var toView: UIView!
     
+    var tutorialView: TutorialView = TutorialView(frame: .zero)
     let headerHeight: CGFloat = 120
-    var masked: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUp()
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -24,77 +25,25 @@ class ViewController: UIViewController {
     }
 
     @IBAction func onTappedStart(_ sender: Any) {
-        startMaskAnimation(in: view, to: toView.frame)
+        tutorialView.start(to: toView.frame)
     }
 
     @IBAction func onTappedClear(_ sender: Any) {
-        clearMaskAnimation(in: view)
+        tutorialView.clear()
     }
 
-    func startMaskAnimation(in view: UIView, to toRect: CGRect) {
-        guard masked == false else { return }
-        masked = true
-        
-        let headerPath = UIBezierPath(rect: CGRect(x: 0, y: 0, width: view.bounds.width, height: headerHeight))
-        
-        let maskLayer = CAShapeLayer()
-        maskLayer.frame = view.bounds
-        maskLayer.fillColor = UIColor.black.cgColor
-        let fromRect = CGRect(x: 0, y: headerHeight, width: view.bounds.width, height: view.bounds.height - headerHeight)
-        let fromPath = UIBezierPath(roundedRect: fromRect, cornerRadius: 1)
-        fromPath.append(headerPath)
-        maskLayer.path = fromPath.cgPath
-        maskLayer.fillRule = CAShapeLayerFillRule.evenOdd
-        view.layer.mask = maskLayer
+}
 
-        let toPath = UIBezierPath(roundedRect: toRect, cornerRadius: toRect.height/2)
-        toPath.append(headerPath)
+extension ViewController {
 
-        let anim = CABasicAnimation(keyPath: "path")
-        anim.fromValue = maskLayer.path
-        anim.toValue = toPath.cgPath
-        anim.duration = 0.2
-        anim.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
-
-        maskLayer.add(anim, forKey: nil)
-
-        CATransaction.begin()
-        CATransaction.setDisableActions(true)
-        CATransaction.setCompletionBlock({
-            maskLayer.path = toPath.cgPath
-            maskLayer.fillRule = CAShapeLayerFillRule.evenOdd
-        })
-        CATransaction.commit()
-    }
-
-    func clearMaskAnimation(in view: UIView) {
-        guard masked == true else { return }
-        masked = false
-
-        let headerPath = UIBezierPath(rect: CGRect(x: 0, y: 0, width: view.bounds.width, height: headerHeight))
-
-        let maskLayer = view.layer.mask as! CAShapeLayer
-
-        let toRect = CGRect(x: 0, y: headerHeight, width: view.bounds.width, height: view.bounds.height - headerHeight)
-        let toPath = UIBezierPath(roundedRect: toRect, cornerRadius: 1)
-        toPath.append(headerPath)
-
-        let anim = CABasicAnimation(keyPath: "path")
-        anim.fromValue = maskLayer.path
-        anim.toValue = toPath.cgPath
-        anim.duration = 0.2
-        anim.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
-
-        maskLayer.add(anim, forKey: nil)
-
-        CATransaction.begin()
-        CATransaction.setDisableActions(true)
-        CATransaction.setCompletionBlock({
-            maskLayer.path = UIBezierPath(roundedRect: view.bounds, cornerRadius: 0).cgPath
-            maskLayer.fillRule = CAShapeLayerFillRule.evenOdd
-        })
-        CATransaction.commit()
-
+    private func setUp() {
+        tutorialView.headerHeight = headerHeight
+        view.addSubview(tutorialView)
+        tutorialView.translatesAutoresizingMaskIntoConstraints = false
+        tutorialView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        tutorialView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        tutorialView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+        tutorialView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
     }
 
 }
